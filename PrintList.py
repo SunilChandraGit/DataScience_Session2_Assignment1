17f0c2a45a9629f662cb7923921d09daa512c1aa
neeil93 <chandrasunil93@gmail.com>
2018-03-27 22:52:45 +0530

    Update
    Update

--- PrintList.py
+++ PrintList.py
@@ -1,5 +1,14 @@
+'''Write a program which accepts a sequence of comma-separated numbers from console
+and generate a list.'''
+
+#Read comma separated list of numbers
 a=input('Please enter a comma separated list of numbers : ');
-b=a.split(',');
+
+#split the input from comma
+numberList=a.split(',');
+
 print('The list of entered number is : ');
-for i in b:
+
+#Print each number in List
+for i in numberList:
     print(i);
